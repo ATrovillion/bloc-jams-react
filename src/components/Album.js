dfaac6f51bcbfd539a354f0594a35fa4c7227268
ATrovillion <aaron.trovillion@gmail.com>
2018-06-25 19:14:39 -0400

    Add album component assignment work

--- src/components/Album.js
+++ src/components/Album.js
@@ -20,21 +20,38 @@ import albumData from './../data/album';
             <section className="album">
                 <section id="album-info">
                     <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
-                <div className="album-details">
-                    <h1 id="album-title">{this.state.album.title}</h1>
-                    <h2 className="artist">{this.state.album.artist}</h2>
-                    <div id="release-info">{this.state.album.releaseInfo}</div>
-                </div>
+                    <div className="album-details">
+                        <h1 id="album-title">{this.state.album.title}</h1>
+                        <h2 className="artist">{this.state.album.artist}</h2>
+                        <div id="release-info">{this.state.album.releaseInfo}</div>
+                    </div>
                 </section>
-                <table id="song-list">
-                    <colgroup>
-                        <col id="song-number-column" />
-                        <col id="song-title-column" />
-                        <col id="song-duration-column" />
-                    </colgroup>
-                    <tbody>
-                    </tbody>
-                </table>
+                <section>
+                    <table id="song-list">
+                        <colgroup>
+                            <col id="song-number-column" />
+                            <col id="song-title-column" />
+                            <col id="song-duration-column" />
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th>Song Number</th>
+                                <th>Song Title</th>
+                                <th>Duration</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.album.songs.map( (song, index) =>
+                                <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{song.title}</td>
+                                <td>{song.duration}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </section>
             </section>
         );
     }
