6070afbd7b2c2ad7942950f4ba716fcfe6eea3e7
Aaron Trovillion <aaron.trovillion@gmail.com>
2018-08-08 13:58:58 -0400

    Add audio playback submission work

--- src/components/Album.js
+++ src/components/Album.js
@@ -13,7 +13,10 @@ import albumData from './../data/album';
         this.state = {
             album: album,
             currentSong: album.songs[0],
-            isPlaying: false
+            isPlaying: false,
+            isHovered: false,
+            hoveredIndex: -1,
+            pausedSong: -1
         };
         
         this.audioElement = document.createElement('audio');
@@ -25,9 +28,10 @@ import albumData from './../data/album';
         this.setState({ isPlaying: true });
     }
 
-    pause() {
+    pause(song) {
         this.audioElement.pause();
         this.setState({ isPlaying: false });
+        this.setState({pausedSong : song})
     }
 
     setSong(song) {
@@ -38,10 +42,49 @@ import albumData from './../data/album';
     handleSongClick(song) {
         const isSameSong = this.state.currentSong === song;
         if (this.state.isPlaying && isSameSong) {
-            this.pause();
+            this.pause(song);
         } else {
             if (!isSameSong) { this.setSong(song); }
             this.play();
+        }
+    }
+
+    handleMouseEnter(index) {
+        this.setState({hoveredIndex: index,
+        isHovered: true})
+    }
+
+    handleMouseLeave() {
+        this.setState({isHovered: false})
+    }
+
+    playPauseNumber(index, song) {
+        if (this.state.isPlaying) {
+            if (/* a song is the currentSong*/ this.state.currentSong === song) {
+                return /*<button>Pause</button>*/ <span className = "ion-pause"></span>
+            } else if (/*a song is not the currentSong*/ this.state.currentSong !== song) {
+                if (/*the song is hovered*/ this.state.isHovered && this.state.hoveredIndex === index) {
+                    return /*<button>Play</button>*/<span className = "ion-play"></span>
+                } else {
+                    return (index + 1)
+                }
+            }
+
+        } else if (/*no song is playing*/ !this.state.isPlaying) {
+                if (/*any song is hovered*/ this.state.isHovered) {
+                    if (/*this song is hovered*/ this.state.hoveredIndex === index) {
+                        return <span className = "ion-play"></span>
+                    } else {
+                        /*a song is not the one hovered*/
+                        return (index +1)
+                    }
+                } else if (this.state.pausedSong === song) {
+                    return <span className = "ion-play"></span>
+                } else {
+                    return (index + 1)
+                }
+        } else {
+            return (index + 1)
         }
     }
 
@@ -51,7 +94,9 @@ import albumData from './../data/album';
                 <section id="album-info">
                     <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
                     <div className="album-details">
+                        <span>
                         <h1 id="album-title">{this.state.album.title}</h1>
+                        </span>
                         <h2 className="artist">{this.state.album.artist}</h2>
                         <div id="release-info">{this.state.album.releaseInfo}</div>
                     </div>
@@ -73,8 +118,11 @@ import albumData from './../data/album';
                         <tbody>
                             {
                                 this.state.album.songs.map( (song, index) =>
-                                <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
-                                <td>{index + 1}</td>
+                                <tr className="desired-song" key={index} onClick={() => this.handleSongClick(song)} 
+                                onMouseEnter={() => this.handleMouseEnter(index)}
+                                onMouseLeave={() => this.handleMouseLeave()}>
+
+                                <td>{this.playPauseNumber(index, song)}</td>
                                 <td>{song.title}</td>
                                 <td>{song.duration}</td>
                                 </tr>
@@ -83,6 +131,7 @@ import albumData from './../data/album';
                     </table>
                 </section>
             </section>
+
         );
     }
  }
