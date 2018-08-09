d3c4d166425a2dfc25cd7b6771aecc2cdb3fc5e2
Aaron Trovillion <aaron.trovillion@gmail.com>
2018-08-09 10:19:08 -0400

    Add player bar range inputs checkpoint work

--- src/components/Album.js
+++ src/components/Album.js
@@ -14,6 +14,8 @@ import PlayerBar from './PlayerBar';
         this.state = {
             album: album,
             currentSong: album.songs[0],
+            currentTime: 0,
+            duration: album.songs[0].duration,
             isPlaying: false
         };
         
@@ -31,10 +33,30 @@ import PlayerBar from './PlayerBar';
         this.setState({ isPlaying: false });
     }
 
+    componentDidMount() {
+        this.eventListeners = {
+            timeupdate: e => {
+                this.setState({ currentTime: this.audioElement.currentTime });
+            },
+            durationchange: e => {
+                this.setState({ duration: this.audioElement.duration });
+            }
+        };
+        this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+        this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+    }
+
+    componentWillUnmount() {
+        this.audioElement.src = null;
+        this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+        this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+    }
+
     setSong(song) {
         this.audioElement.src = song.audioSrc;
         this.setState({ currentSong: song });
     }
+
 
     handleSongClick(song) {
         const isSameSong = this.state.currentSong === song;
@@ -60,6 +82,12 @@ import PlayerBar from './PlayerBar';
         const newSong = this.state.album.songs[newIndex];
         this.setSong(newSong);
         this.play();
+    }
+
+    handleTimeChange(e) {
+        const newTime = this.audioElement.duration * e.target.value;
+        this.audioElement.currentTime = newTime;
+        this.setState({ currentTime: newTime });
     }
 
     render() {
@@ -101,9 +129,12 @@ import PlayerBar from './PlayerBar';
                     <PlayerBar
                         isPlaying={this.state.isPlaying}
                         currentSong={this.state.currentSong}
+                        currentTime={this.audioElement.currentTime}
+                        duration={this.audioElement.duration}
                         handleSongClick={() => this.handleSongClick(this.state.currentSong)}
                         handlePrevClick={() => this.handlePrevClick()}
                         handleNextClick={() => this.handleNextClick()}
+                        handleTimeChange={(e) => this.handleTimeChange(e)}
                     />
                 </section>
             </section>
